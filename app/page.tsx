2f9429ff142af0b7a584488e8aa97cc8d60ee6e9
Abdullahi onikoko <onikokoabdullahi@gmail.com>
2025-02-07 23:14:49 +0000

    Removed locomotive scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import Head from "next/head";
-import { useEffect } from "react";
-import type LocomotiveScroll from "locomotive-scroll";
 
 import Hero from "@/components/hero";
 import ScrollToTop from "@/components/scrollToTop";
@@ -17,23 +15,6 @@ import Contact from "@/components/contact";
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      let locomotiveScroll: LocomotiveScroll | null = null;
-
-      (async () => {
-        const LocomotiveScroll = (await import("locomotive-scroll")).default;
-        locomotiveScroll = new LocomotiveScroll();
-      })();
-
-      return () => {
-        if (locomotiveScroll) {
-          locomotiveScroll.destroy(); // Cleanup
-        }
-      };
-    }
-  }, []);
-
   return (
     <>
       <Head>

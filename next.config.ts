1a93585a8d91586924e897ab0646483a8d0ed63d
Abdullahi onikoko <onikokoabdullahi@gmail.com>
2025-02-07 17:46:27 +0000

    Fixe cache issue

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "photos.zillowstatic.com",
-        port: "",
-        search: "",
-      },
-      {
-        protocol: "https",
-        hostname: "thumbs.dreamstime.com",
-        port: "",
-        search: "",
-      },
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
